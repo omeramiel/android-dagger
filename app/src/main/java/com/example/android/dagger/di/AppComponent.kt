521afbb9fcbd793b996b4e081750af03ba660bb0
Omer Amiel <omeramiel@gmail.com>
2019-11-06 11:39:09 +0200

    logged in user scope

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -2,8 +2,9 @@ package com.example.android.dagger.di
 
 import android.content.Context
 import com.example.android.dagger.login.LoginComponent
-import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.user.UserComponent
+import com.example.android.dagger.user.UserManager
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -17,9 +18,11 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
+    fun userManager(): UserManager
+
     fun registrationFactory(): RegistrationComponent.Factory
 
     fun loginFactory(): LoginComponent.Factory
 
-    fun inject(activity: MainActivity)
+    fun userFactory(): UserComponent.Factory
 }
